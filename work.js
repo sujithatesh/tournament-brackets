b35bdba3b13d87bd8b87504dceed306d1752bbf3
Sujith <sujithatesh@gmail.com>
2023-04-02 14:36:38 +0530

    Added div elements when button is clicked, added id to levels, made separate divs for levels

--- work.js
+++ work.js
@@ -1,4 +1,6 @@
 let noOfTeams = prompt("How many number of teams are participating in the tournament?");
+let match = document.getElementById("match");
+
 while(1){
     if(noOfTeams==1){
         noOfTeams = prompt("Enter more than one team");
@@ -14,16 +16,41 @@ while(1){
         console.log(i);
     }
 }
-let noOfClicks = 0;
 
-console.log(noOfTeams);
+let noOfClicks = 0;
+let level = 0;
+let initLevel =(Math.log2(noOfTeams)-noOfClicks);
+
+let oldId = document.getElementById("level");
+oldId.setAttribute("id","level"+initLevel);
+
+
 function winner_bracket(){
     var winner = document.getElementById(down);
     winner.classList.add("winner");
 }
+
+function createNewLevel(level){
+    if(level<0)
+        return;
+    var newLevelDiv = document.createElement("div");
+    newLevelDiv.classList.add("level");
+    newLevelDiv.textContent = "Hello";
+    newLevelDiv.id = "level"+(level);
+    match.appendChild(newLevelDiv);
+}
+
 function createNewBracket(){
     noOfClicks++;
+    let oldLevel = level;
+    level = (Math.log2(noOfTeams)-noOfClicks);
+    if(oldLevel!=level){
+        createNewLevel(level);
+    }
+    if(level<0)
+        return;
     const newBracket = document.getElementById('b1').cloneNode(true); 
-    newBracket.id = "b"+(Math.log2(noOfTeams)-noOfClicks);
-    document.body.appendChild(newBracket);
+    newBracket.id = "b"+(noOfClicks);
+    let Divlevel = document.getElementById('level'+level);
+    Divlevel.appendChild(newBracket);
 }
