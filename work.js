c48560e9c15c8acf7109dd2d4406e481fd94b3ba
Sujith <sujithatesh@gmail.com>
2023-11-12 20:57:13 +0530

    no more prompts, toggling winner works

--- work.js
+++ work.js
@@ -1,38 +1,49 @@
-let noOfTeams = prompt("How many number of teams are participating in the tournament?");
-let match = document.getElementById("match");
+let noOfTeams = 0;
+let bracketNo= 0;
+let level = 0;
+let initLevel = 0;
 
-while(1){
-    if(noOfTeams==1){
-        noOfTeams = prompt("Enter more than one team");
+
+function createMatch(){
+    var tempLevel = initLevel; 
+    while(tempLevel>1){
+        tempLevel--;
+        createNewLevel(tempLevel); 
+        console.log(tempLevel);
     }
-    if(noOfTeams>1&&noOfTeams!==""){
-        let i = noOfTeams & (noOfTeams-1);
-        if(i!=0){
-            noOfTeams = prompt("Enter correct number of teams( order of 2^n )");
-        }
-        else{
-            break;
-        }
-        console.log(i);
-    }
+    console.log("hello");
 }
 
 
+function printlog(){
+    noOfTeams= parseInt(document.getElementById("noTeams").value);
+    console.log("Teams",noOfTeams)
+    initLevel = Math.floor(Math.log2(noOfTeams)) - bracketNo + 1;
+    console.log("init",initLevel)
+    createMatch()
+    document.getElementById("level0").classList.add("hide");
+}
 
-
-let bracketNo= 0;
-let level = 0;
-let initLevel =(Math.log2(noOfTeams)-bracketNo);
+console.log("noTeams",initLevel)
+console.log("init",initLevel)
+console.log("brack",bracketNo)
 
 let oldLevelId = document.getElementById("level");
 oldLevelId.setAttribute("id","level"+initLevel);
+
 let initBracketId = document.getElementById("b1");
 initBracketId.setAttribute("id",initLevel+"-"+"0");
+console.log("initbrac",initBracketId)
 
 
 function winner_bracket(){
-    var winner = document.getElementById(down);
-    winner.classList.add("winner");
+    const clickedBracket = event.target;
+    const parentDiv = clickedBracket.parentElement;
+
+    const buttons = parentDiv.querySelectorAll(".bracket");
+    buttons.forEach(button => button.classList.remove("winner"));
+
+    clickedBracket.classList.toggle("winner");
 }
 
 function createNewLevel(level){
@@ -42,6 +53,7 @@ function createNewLevel(level){
     newLevelDiv.classList.add("level");
     newLevelDiv.textContent = "level: "+level;
     newLevelDiv.id = "level"+(level);
+    console.log(newLevelDiv)
     let bracketsPerLevel = 2**(level-1);
     var bracketNo = 0;
     while(bracketsPerLevel--){
@@ -54,26 +66,22 @@ function createNewLevel(level){
 
 function createNewBracket(level,bracketNo){
     if(level<1)
-        return;
-    const newBracket = document.getElementById(initLevel+"-"+"0").cloneNode(true); 
+    return;
+    let newBracket = null;
+    console.log("initlevel",initLevel);
+    newBracket = document.getElementById("0-0").cloneNode(true); 
+    console.log("newbrac",newBracket)
     newBracket.id = level+bracketNo;
     let Divlevel = document.getElementById('level'+level);
     if (Divlevel) {
-        console.log(Divlevel.id);
+        console.log("divlevel",Divlevel.id);
         Divlevel.appendChild(newBracket);
     } 
     else {
         console.error('Could not find element with id "level' + level + '".');
     }
-    //console.log(Divlevel.id);
-    //Divlevel.appendChild(newBracket);
 }
 
-function createMatch(){
-    var tempLevel = initLevel; 
-    while(tempLevel>1){
-        tempLevel--;
-        createNewLevel(tempLevel); 
-        console.log(tempLevel);
-    }
-}
+
+
+
