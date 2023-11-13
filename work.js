254a2d6a9e37e3406d580fdf3e99ca9ddc34ddc8
Sujith <sujithatesh@gmail.com>
2023-11-13 20:01:39 +0530

    soo much shit, done with the project, need to add some more qol functionality, maybe too much of a linear method, need to implement it with 2d arrays(much easier)

--- work.js
+++ work.js
@@ -1,14 +1,12 @@
-let noOfTeams = 0;
-let vsNo= 0;
-let level = 0;
-let initLevel = 0;
 let Teams = []
 
 
 function start(){ 
-    noOfTeams = Teams.length
-    initLevel = Math.floor(Math.log2(noOfTeams)) - vsNo + 1;
-    createMatch();
+    let noOfTeams = Teams.length
+    initLevel = Math.floor(Math.log2(noOfTeams)) + 1;
+    const match = document.getElementById("match");
+    match.innerHTML = "";
+    createMatch(initLevel);
 }
 
 function addTeam(){
@@ -28,7 +26,7 @@ function deleteTeam(){
     printTeams.removeChild(printTeams.lastChild);
 }
 
-function createMatch(){
+function createMatch(initLevel){
     var tempLevel = initLevel; 
     while(tempLevel>1){
         tempLevel--;
@@ -43,52 +41,83 @@ function createNewLevel(level){
     var newLevelDiv = document.createElement("div");
     newLevelDiv.classList.add("level");
     newLevelDiv.textContent = "level: "+level;
-    newLevelDiv.id = "level"+(level);
-    let noOfVs = 2 ** (level-1);
-    while(noOfVs--){
-        requestAnimationFrame(() => createNewVs(vsNo, newLevelDiv));
-        vsNo++;
+    newLevelDiv.id = level;
+    for(let noOfVs = 2 ** (level-1); noOfVs > 0; noOfVs--){
+        requestAnimationFrame(() => createNewVs(noOfVs * 2, newLevelDiv, level));
+        console.log("noOfVs",noOfVs);
     }
     match.appendChild(newLevelDiv);
 }
 
-function createNewVs(order, newLevelDiv){
+function createNewVs(noOfVs, newLevelDiv, level){
+    let noOfTeams = Teams.length
+    let initLevel = Math.floor(Math.log2(noOfTeams)) + 1;
     let newVs = document.createElement("div");
     newVs.classList.add("vs");
-    newVs.id = "b" + order;
-    requestAnimationFrame(() => createNewBracket(order, "up", newVs));
-    requestAnimationFrame(() => createNewBracket("this", "down", newVs));
-    newLevelDiv.append(newVs);
+    newVs.id = "vs" + noOfVs;
+    console.log("init",initLevel);
+    if(newLevelDiv.id == initLevel - 1){
+        requestAnimationFrame(() => createNewBracket([noOfVs - 1], newVs, true, level));
+        requestAnimationFrame(() => createNewBracket([noOfVs - 2], newVs, true, level));
+    }
+    else{
+        requestAnimationFrame(() => createNewBracket([noOfVs - 1], newVs, false, level));
+        requestAnimationFrame(() => createNewBracket([noOfVs - 2], newVs, false, level));
+    }
+     newLevelDiv.append(newVs);
 }
 
 
-function createNewBracket(inner, upOrDown, newVs){
+function createNewBracket(index, newVs, show, level){
     let newBracket = document.createElement("button");
     newBracket.classList.add("bracket");
+    newBracket.id = level + "s" + index.toString()
     newBracket.addEventListener("click",winner_bracket);
-    newBracket.innerHTML = inner;
-    switch (upOrDown) {
-        case "up":
-            newBracket.id = "up";
-            break;
-        case "down":
-            newBracket.id = "down";
-            break;
-        default:
-            break;
+    if(show){
+        newBracket.innerHTML = Teams[index];
+    }
+    else{
+        newBracket.innerHTML = "";
     }
     newVs.append(newBracket);
 }
 
 
 function winner_bracket(){
+
     const clickedBracket = event.target;
-    const parentDiv = clickedBracket.parentElement;
+    const vsDiv = clickedBracket.parentElement;
+    const parentLevelDiv = vsDiv.parentElement;
+    let parentLevelId = parseInt((parentLevelDiv.id));
 
-    const buttons = parentDiv.querySelectorAll(".bracket");
+    if(parentLevelId == 1){
+        const buttons = vsDiv.querySelectorAll(".bracket");
+        buttons.forEach(button => button.classList.remove("final"));
+        clickedBracket.classList.toggle("final");
+        return;
+    }
+
+    const buttons = vsDiv.querySelectorAll(".bracket");
     buttons.forEach(button => button.classList.remove("winner"));
-
     clickedBracket.classList.toggle("winner");
+
+    let clickedBracketId = clickedBracket.id.split("s")[1];
+    const nextLevel = document.getElementById((parentLevelId - 1).toString());
+    const nextWinnerId = nextLevel.id + "s" +Math.floor(parseInt(clickedBracketId) / 2).toString();
+    const nextWinner = document.getElementById(nextWinnerId)
+    nextWinner.innerHTML = clickedBracket.innerHTML;
+    console.log(nextWinner);
 }
 
 
+
+// -------------------------------------------------------------------------
+
+var input = document.getElementById("inputTeams");
+
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("add").click();
+  }
+});
